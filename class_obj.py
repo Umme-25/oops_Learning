a8a08a23b1bf0102a1a6cc73f2a5bbc479fa5dae
Umme-25 <ummehabiba2241@gmail.com>
2026-06-10 14:18:13 +0530

    Start

--- class_obj.py
+++ class_obj.py
@@ -3,5 +3,10 @@
 objects => a container
     => data-> attributes
     => functionality -> methods/behaviour
-    
+
 """
+
+fruits = ["mango", "apple", "orange"]
+print(type(fruits))
+#fruits is an object of a class
+fruits.append("grapes")
